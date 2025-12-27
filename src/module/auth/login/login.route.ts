878c0a8684c5865561371b5fcb3a2e39f973ad85
Limon Reza <limonreza3.1416@gmail.com>
2025-12-27 23:51:07 +0600

    minor changes

--- src/module/auth/login/login.route.ts
+++ src/module/auth/login/login.route.ts
@@ -1,7 +1,5 @@
 import express from "express";
 import { loginController } from "./login.controller";
-import auth from "../../../middleware/auth";
-import { Role } from "../../../types/roles.type";
 
 const router = express.Router();
 router.post("/api/v1/auth/signin", loginController.loginUsers);
